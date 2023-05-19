6accc4543c937c7b6663fd48c648448f1ab0383d
mcalgul <cihadalgul003@gmail.com>
2023-05-20 00:09:06 +0300

    proje eklendi

---
+++ Kütüphane_Otomasyonu/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kütüphane_Otomasyonu
+{
+    public partial class Form1 : Form
+    {
+        public string KullaniciAdi;
+        public string Sifre;
+        public bool sonuc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == null || textBox2.Text == null)
+            {
+                MessageBox.Show("Lütfen Kullanıcı Adınızı ve Şifrenizi Girin!");
+            }
+            else
+            {
+                KullaniciAdi = textBox1.Text;
+                Sifre = textBox2.Text;
+                logControl(KullaniciAdi, Sifre);
+            }
+        }
+        private void logControl(string KullaniciAdi, string Sifre)
+        {
+            sonuc = B10.Admin(KullaniciAdi, Sifre);
+            if (sonuc == false)
+            {
+                MessageBox.Show("Kullanıcı adı yada şifre yanlış!!!");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            else
+            {
+                Form1 kapat = new Form1();
+                kapat.Close();
+                Form2 yeni = new Form2();
+                yeni.Show();
+                this.Hide();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Kütüphane_Otomasyonu/Properties/ConnDatebase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kütüphane_Otomasyonu.Properties
+{
+    internal class ConnDatebase
+    {
+        public static string homeUrl = @"C:\Users\mcaal\OneDrive\Masaüstü\Kutuphane-Otomasyonu\KütüphaneBilgileri.mdb";
+    }
+}
